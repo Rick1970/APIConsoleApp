6482a420388b41d7c802b5ebd2af517f349e92b5
rthor <rthor@LAPTOP-343NT6OO>
2016-10-12 11:17:22 -0700

    console app lesson completed.

--- src/APIConsoleApp/Program.cs
+++ src/APIConsoleApp/Program.cs
@@ -1,14 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
-namespace APIConsoleApp
+namespace TwilioTest
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        public class Message
         {
+            public string To { get; set;}
+            public string From {get; set;}
+            public string Body {get; set;}
+            public string Status {get; set;}
+        }
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            //1
+            var request = new RestRequest("Accounts/AC55a42b8c4fd54b53488c1b1f7a2f00f9/Messages.json", Method.GET);
+            client.Authenticator = new HttpBasicAuthenticator("AC55a42b8c4fd54b53488c1b1f7a2f00f9", "3f58b4431a4a20d89d49c31832bdac52");
+            //2
+            var response = new RestResponse();
+            //3a
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            //4
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var messageList = JsonConvert.DeserializeObject<List<Message>>(jsonResponse["messages"].ToString());
+            foreach (var message in messageList)
+            {
+                Console.WriteLine("To: {0}", message.To);
+                Console.WriteLine("From: {0}", message.From);
+                Console.WriteLine("Body: {0}", message.Body);
+                Console.WriteLine("Status: {0}", message.Status);
+            }
+            Console.ReadLine();
+        }
+
+        //3b
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
         }
     }
 }
